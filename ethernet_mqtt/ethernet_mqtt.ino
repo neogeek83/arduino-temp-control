c06b6e7454b2b55951cf17599993c29507c2c506
Daniel Neal <danielneal@gmail.com>
2017-03-13 19:23:56 -0500

    Added code for Ethernet Shield MQTT as well as notes on using mosquitto. Code successfully works on Ethernet Shield, can subscribe to multiple topics and publish as necessary.

---
+++ ethernet_mqtt/ethernet_mqtt.ino
@@ -0,0 +1,85 @@
+/*   https://github.com/knolleary/pubsubclient/
+ Basic MQTT example
+
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress ip(10, 0, 0, 65);
+IPAddress server(10, 0, 0, 24);
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  char temp[length+1];
+  strncpy(temp, (char*)payload, length);
+  temp[length]='\0';
+    
+  Serial.print(temp);
+  client.publish("outTopic",temp,length);  
+  Serial.println();
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic","hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+      client.subscribe("inTopic2");
+      client.subscribe("inTopic3");
+      client.subscribe("inTopic4");
+      client.subscribe("inTopic5");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(57600);
+
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+  Ethernet.begin(mac, ip);
+  // Allow the hardware to sort itself out
+  delay(1500);
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
