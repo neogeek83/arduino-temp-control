7a9806d942b5e2559835490ced945cebe1cc804a
thefirstnutzo <danielneal@gmail.com>
2017-05-15 23:19:30 +0000

    Create setup_pi.sh

---
+++ ethernet_mqtt/setup_pi.sh
@@ -0,0 +1,26 @@
+# Initial Setup
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y vim git
+
+# Install Mosquitto
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+apt-key add mosquitto-repo.gpg.key
+rm mosquitto-repo.gpg.key
+cd /etc/apt/sources.list.d/
+wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
+apt-get update
+apt-get install -y mosquitto mosquitto-clients
+
+# Install HomeAssistant
+apt-get install python3 python3-venv python3-pip
+useradd -rm homeassistant
+cd /srv
+mkdir homeassistant
+chown homeassistant:homeassistant homeassistant
+su -s /bin/bash homeassistant
+cd /srv/homeassistant
+python3 -m venv .
+source bin/activate
+pip3 install homeassistant 
