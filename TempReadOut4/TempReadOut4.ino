a2801c513d6cdddfafe74b305b4979ef99cb756e
Kenny <neogeek83@gmail.com>
2018-01-25 19:59:09 -0500

    Trimming code down to not have IR transmitter nor LCD unit, just MQTT, Ethernet and 2 relays

--- TempReadOut4/TempReadOut4.ino
+++ TempReadOut4/TempReadOut4.ino
@@ -1,39 +1,3 @@
-/*
- The LiquidCrystal library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface. -- Wire up using the below
-   Better wiring diiagram(just use data pins defined below): http://www.instructables.com/id/How-to-use-an-LCD-displays-Arduino-Tutorial/step3/The-Circuit/
-
-  The circuit:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 8
- * LCD D4 pin to digital pin 9
- * LCD D5 pin to digital pin 10
- * LCD D6 pin to digital pin 11
- * LCD D7 pin to digital pin 12
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K potentiometer:
- * Ends to +5V and ground
- * LCD VO pin (pin 3) to potentiometer center lead
- * 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
-// include the library code:
-#include <LiquidCrystal.h>
 #include <SPI.h>
 #include <Ethernet.h> //NEEDS DIGITAL PINS 10,11,12,13
 #include <PubSubClient.h>
@@ -50,7 +14,7 @@ PubSubClient client(ethClient);
 //  DEBUG OPTION :    //
 ////////////////////////
 // use DEBUG_ON for debugging (requires serial connection on startup)
-#define DEBUG_OFF
+#define DEBUG_ON
 
 
 #ifdef DEBUG_ON
@@ -66,9 +30,9 @@ PubSubClient client(ethClient);
 //////////////////
 // RELAY SETTINGS
 //////////////////
-int relay_pins[]    = {  4, A5 };
+int relay_pins[]    = {  A1, A2 };
 //starting state
-int relay_state[]   = { LOW, LOW }; //LOW=OFF, HIGH=ON
+int relay_state[]   = { LOW, LOW }; //Default, LOW=OFF, HIGH=ON
 
 //Relay output Mappings(name to index in array relay_pins)
 #define HEAT_RELAY 0
@@ -133,18 +97,11 @@ void reconnect() {
   }
 }
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 5, 6, A0);
 
 #include "idDHTLib.h"
 
 int DHTpin = 2; //Digital pin for comunications
 idDHTLib DHTLib(DHTpin, idDHTLib::DHT22);
-
-#include "IRremote.h"
-int receiver = 3; // Signal Pin of IR receiver to Arduino Digital Pin 3
-IRrecv irrecv(receiver);     // create instance of 'irrecv'
-decode_results results;      // create instance of 'decode_results'
 
 enum mode{AC=0,HEAT=1,HUMIDIFIER=2,DEHUMIDIFIER=3};
 
@@ -170,11 +127,6 @@ void setup() {
      pinMode(relay_pins[i], OUTPUT);
      digitalWrite(relay_pins[i], relay_state[i]);
   }
-  
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-
-  irrecv.enableIRIn(); // Start the receiver
 
   #ifdef DEBUG_ON
     Serial.begin(57600);
@@ -195,64 +147,29 @@ void loop() {
   client.loop();
   publishTempHum();
 
-  
-  lcd.setCursor(0, 0); // setCursor(col:0-16,row:0-1)
 
   int result = DHTLib.acquireAndWait();
   switch (result) {
-  case IDDHTLIB_OK: 
-    //lcd.println("OK"); 
-    if (irrecv.decode(&results)){ // have we received an IR signal?
-      String IRVal = translateIR();
-      
-      if ( IRVal == ">>|" ) setHumidity++;
-      if ( IRVal == "|<<" ) setHumidity--;
-      
-      if ( IRVal == "VOL+" ) setTemp++;
-      if ( IRVal == "VOL-" ) setTemp--;
-
-      if ( IRVal == "0" ) overrideOn=false;
-      if ( IRVal == "1" ) overrideOn=true;
-      
-      if ( IRVal == "4" ) { setRelay(FAN_RELAY,false); DEBUG_PRINT("FAN_RELAY: ");printRelayState(FAN_RELAY);}
-      if ( IRVal == "7" ) { setRelay(FAN_RELAY,true); DEBUG_PRINT("FAN_RELAY: ");printRelayState(FAN_RELAY);}
-      
-      if ( IRVal == "6" ) thresholdEnabled=false;
-      if ( IRVal == "9" ) thresholdEnabled=true;
-      
-      if ( IRVal == "FUNC/STOP" ) localControl=!localControl;
-      
-      DEBUG_PRINT("Button pressed: ");
-      DEBUG_PRINTLN(IRVal);  
-      
-      irrecv.resume(); // receive the next value
-    }
+  case IDDHTLIB_OK:
     readSensors();
-    writeToLCD();
     controlRelays();
     break;
-  case IDDHTLIB_ERROR_CHECKSUM: 
-    //lcd.println("Error:Checksum error"); 
+  case IDDHTLIB_ERROR_CHECKSUM:
     //DEBUG_PRINTLN("Error:Checksum error");
     break;
   case IDDHTLIB_ERROR_TIMEOUT: 
-    //lcd.println("Error:Interrupt time out error"); 
     //DEBUG_PRINTLN("Error:Interrupt time out error");
     break;
   case IDDHTLIB_ERROR_ACQUIRING: 
-    lcd.println("Error:Acquiring"); 
     DEBUG_PRINTLN("Error:Acquiring"); 
     break;
   case IDDHTLIB_ERROR_DELTA: 
-    lcd.println("Error:Delta time to small");
     DEBUG_PRINTLN("Error:Delta time to small"); 
     break;
   case IDDHTLIB_ERROR_NOTSTARTED: 
-    lcd.println("Error:Not started"); 
     DEBUG_PRINTLN("Error:Not started"); 
     break;
   default: 
-    lcd.println("Error:Unknown"); 
     DEBUG_PRINTLN("Error:Unknown"); 
     break;
   }
@@ -269,53 +186,15 @@ void publishTempHum(){
   client.publish("NodeMCU1/temp",temp);
   client.publish("NodeMCU1/hum",hum);
   
-  //DEBUG_PRINT("NodeMCU1/temp:");
-  //DEBUG_PRINTLN(temp);
-  //DEBUG_PRINT("NodeMCU1/hum:");
-  //DEBUG_PRINTLN(hum);
+  DEBUG_PRINT("NodeMCU1/temp:");
+  DEBUG_PRINTLN(temp);
+  DEBUG_PRINT("NodeMCU1/hum:");
+  DEBUG_PRINTLN(hum);
 }
 
 void readSensors(){
   curTempF = (int)DHTLib.getFahrenheit();
   curHumidity = (int)DHTLib.getHumidity();
-}
-
-void writeToLCD(){
-  int tempF = curTempF;
-  int humidity = curHumidity;
-  lcd.print("Temp:");
-  DEBUG_PRINT("Temp:");
-  lcd.print(tempF); lcd.print("F|");
-  DEBUG_PRINT(tempF); DEBUG_PRINT("F|");
-  
-
-  if (opMode == HEAT || opMode == AC) {
-    lcd.print(setTemp);
-    lcd.print("F");
-    DEBUG_PRINT(setTemp);
-    DEBUG_PRINTLN("F");
-  } else {
-    lcd.print(setHumidity);
-    lcd.print("%");
-    DEBUG_PRINT(setHumidity);
-    DEBUG_PRINTLN("%");
-  }
-  lcd.setCursor(15, 0);
-  if(localControl){
-    lcd.print("L");
-  } else {
-    lcd.print("R");
-  }
-  
-  lcd.setCursor(0, 1);
-  lcd.print("Humidity:");
-  lcd.print(humidity); lcd.print("%|");
-  
-  DEBUG_PRINT("Humidity:");
-  DEBUG_PRINT(humidity); DEBUG_PRINT("%|");
-
-
-  printRelayState(0);
 }
 
 void controlRelays(){
@@ -411,63 +290,14 @@ void setRelay(int i, boolean on){
    //DEBUG_PRINT(relay_pins[i]);
    //DEBUG_PRINT(" turned ");
    //DEBUG_PRINT(relay_state[i]);
+   DEBUG_PRINT("RELAY_");DEBUG_PRINT(i);DEBUG_PRINT(": ");printRelayState(FAN_RELAY);
 }
 
 void printRelayState(int i){
    if (relay_state[i]){
-     lcd.print("ON ");
      DEBUG_PRINTLN("ON ");
    } else {
-     lcd.print("OFF");
      DEBUG_PRINTLN("OFF");
    }
 }
 
-String translateIR(){ // takes action based on IR code received
-
-// describing Remote IR codes 
-  switch(results.value) {
-    //row 1
-    case 0xFFA25D: return "PWR";
-    case 0xFF629D: return "VOL+";
-    case 0xFFE21D: return "FUNC/STOP";
-  
-    //row 2
-    case 0xFF22DD: return "|<<";
-    case 0xFF02FD: return "PLAY/PAUSE";
-    case 0xFFC23D: return ">>|";
-  
-    //row 3
-    case 0xFFE01F: return "DOWN";
-    case 0xFFA857: return "VOL-";
-    case 0xFF906F: return "UP";
-  
-    //row 4
-    case 0xFF6897: return "0";
-    case 0xFF9867: return "EQ";
-    case 0xFFB04F: return "ST/REPT";
-  
-    //row 5
-    case 0xFF30CF: return "1";
-    case 0xFF18E7: return "2";
-    case 0xFF7A85: return "3";
-  
-    //row 6
-    case 0xFF10EF: return "4";
-    case 0xFF38C7: return "5";
-    case 0xFF5AA5: return "6";
-  
-    //row 7
-    case 0xFF42BD: return "7";
-    case 0xFF4AB5: return "8";
-    case 0xFF52AD: return "9";
-  
-    //holding a button down results in a repeat code
-    case 0xFFFFFFFF: return "REPEAT";
-
-    default: 
-      String retValue = "0x";
-      retValue = retValue + String(results.value, HEX);
-      return retValue;
-  }// End Case
-} //END translateIR
